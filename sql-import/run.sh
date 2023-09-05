ea4e8da8b261c89a5ab3740d4d90685f67b6af41
Veronika Gillies <veronikamwinters@gmail.com>
2023-09-05 17:15:18 +0000

    fix paths

--- sql-import/run.sh
+++ sql-import/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 mkdir -p garden
-git clone https://github.com/codegod100/daily.git garden/vera
+git clone https://github.com/codegod100/daily.git data/garden/vera
 cd garden/vera
 git pull
 cd /workspace/sql-import
-export GARDEN_DIR=/workspace/sql-import/garden
 node sqlite-import.js
 sleep 30 #so we don't thrash on restart
